a36b8bcf7401ee6b73298f8705cbeff237a7f082
Vadim Vadimov <berendeyzhiv@gmail.com>
2021-03-04 22:49:24 +0300

    Removed '__main__' condition

--- INTERACTION/__init__.py
+++ INTERACTION/__init__.py
@@ -10,9 +10,3 @@ Interaction.goto.__doc__ = 'User behavior patterns and bot reaction to them'
 Interaction.default.__doc__ = 'Default behavior of the robot'
 
 interaction = Interaction(BOT_PHRASES, GOTO, DEFAULT)
-
-if __name__ == '__main__':
-    
-    for field in interaction._fields:
-        doc = getattr(Interaction, field)
-        print(f'Interaction.{field}.__doc__ : {doc.__doc__}')
